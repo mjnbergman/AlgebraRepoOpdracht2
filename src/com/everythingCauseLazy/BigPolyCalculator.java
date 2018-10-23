7d97c3a8a53ed269829dc357eae2f15e434e09fa
Bessems <dwmbessems@hotmail.com>
2018-10-23 17:42:53 +0200

    line indentation fix

--- src/com/everythingCauseLazy/BigPolyCalculator.java
+++ src/com/everythingCauseLazy/BigPolyCalculator.java
@@ -82,7 +82,7 @@ public class BigPolyCalculator {
 	}
 	
 	
-public static int[] sub(int[] p1, int[] p2, int mod) {
+	public static int[] sub(int[] p1, int[] p2, int mod) {
 		
 		int counter = 0;
 		int index = 0;
@@ -125,122 +125,122 @@ public static int[] sub(int[] p1, int[] p2, int mod) {
 		
 		return moduloPoly(output, mod);
 	}
-public static int[][] longPolyDivision(int[] poly1, int[] poly2, int mod) {
-	int degree1 = poly1.length - 1;
-	int degree2 = poly2.length - 1;
+	public static int[][] longPolyDivision(int[] poly1, int[] poly2, int mod) {
+		int degree1 = poly1.length - 1;
+		int degree2 = poly2.length - 1;
+		
+		int[] output = new int[poly1.length];
+		int[] remainder = poly1;
+		int[][] returnable = new int[2][poly1.length];
+		
+		int q = 0;
+		
+		int[] r = poly1;
 	
-	int[] output = new int[poly1.length];
-	int[] remainder = poly1;
-	int[][] returnable = new int[2][poly1.length];
-	
-	int q = 0;
-	
-	int[] r = poly1;
-
-	
-	int polyIndex = 0;
-	
-	while(degree1 >= degree2) {
 		
-		int lcDegree = 0;
-		int lcDegree2 = 0;
+		int polyIndex = 0;
 		
-		int lc1 = 0;
-		int lc2 = 0;
-		
-		for(int i = 0; i < poly2.length; i++) {
-			if(poly2[i] != 0) {
-				lcDegree = poly2.length - i - 1;
-				lc2 = poly2[i];
-				break;
-			}
-		}
-		for(int i = 0; i < poly1.length; i++) {
-			if(poly1[i] != 0) {
-				lcDegree2 = poly1.length - i - 1;
-				lc1 = poly1[i];
-				break;
-			}
-		}
-		
-		if(lc1 < lc2) {
-			break;
-		}
-		
-		System.out.println("De degree van pol1 is: " + lcDegree2 + " en van pol2 is: " + lcDegree + " en pol lengte 1 is " + poly1.length + "en pol lengte 2 is: " + poly2.length + " en lc1 is " + lc1 + " en lc2 is " + lc2);
-		
-		int oDegree = output.length - (lcDegree2 - lcDegree) - 1;
-		
-		output[oDegree] += Math.floorDiv(lc1, lc2);
-		
-		System.out.println("Long division de vermenigvuldiging is " + Math.floorDiv(lc1, lc2) + "x^" + (lcDegree2 - lcDegree));
-		
-		for(int i = 0; i < poly2.length; i++) {
+		while(degree1 >= degree2) {
 			
-			if(poly2[i] == 0) {
-				continue;
+			int lcDegree = 0;
+			int lcDegree2 = 0;
+			
+			int lc1 = 0;
+			int lc2 = 0;
+			
+			for(int i = 0; i < poly2.length; i++) {
+				if(poly2[i] != 0) {
+					lcDegree = poly2.length - i - 1;
+					lc2 = poly2[i];
+					break;
+				}
+			}
+			for(int i = 0; i < poly1.length; i++) {
+				if(poly1[i] != 0) {
+					lcDegree2 = poly1.length - i - 1;
+					lc1 = poly1[i];
+					break;
+				}
 			}
 			
-			int outputDegree = poly1.length - (lcDegree2 - lcDegree + (poly2.length - i));
-			poly1[outputDegree] -= Math.floorDiv(lc1, lc2) * poly2[i];
-			
-			System.out.println("De output index is: " + outputDegree + " en het getal is min " + Math.floorDiv(lc1, lc2) * poly2[i]);
-		}
-		
-		for(int i = 0; i < poly1.length; i++) {
-			if(poly1[i] != 0) {
-				degree1 = poly1.length - i - 1;
+			if(lc1 < lc2) {
 				break;
 			}
-		}
-	
-		degree2 = lcDegree;
+			
+			System.out.println("De degree van pol1 is: " + lcDegree2 + " en van pol2 is: " + lcDegree + " en pol lengte 1 is " + poly1.length + "en pol lengte 2 is: " + poly2.length + " en lc1 is " + lc1 + " en lc2 is " + lc2);
+			
+			int oDegree = output.length - (lcDegree2 - lcDegree) - 1;
+			
+			output[oDegree] += Math.floorDiv(lc1, lc2);
+			
+			System.out.println("Long division de vermenigvuldiging is " + Math.floorDiv(lc1, lc2) + "x^" + (lcDegree2 - lcDegree));
+			
+			for(int i = 0; i < poly2.length; i++) {
+				
+				if(poly2[i] == 0) {
+					continue;
+				}
+				
+				int outputDegree = poly1.length - (lcDegree2 - lcDegree + (poly2.length - i));
+				poly1[outputDegree] -= Math.floorDiv(lc1, lc2) * poly2[i];
+				
+				System.out.println("De output index is: " + outputDegree + " en het getal is min " + Math.floorDiv(lc1, lc2) * poly2[i]);
+			}
+			
+			for(int i = 0; i < poly1.length; i++) {
+				if(poly1[i] != 0) {
+					degree1 = poly1.length - i - 1;
+					break;
+				}
+			}
 		
-	}
-	
-	if(mod == -1) {
-		returnable[0] = output;
-		returnable[1] = poly1;
+			degree2 = lcDegree;
+			
+		}
+		
+		if(mod == -1) {
+			returnable[0] = output;
+			returnable[1] = poly1;
+			
+			return returnable;
+		}
+		
+		returnable[0] = moduloPoly(output, mod);
+		returnable[1] = moduloPoly(poly1, mod);
 		
 		return returnable;
 	}
-	
-	returnable[0] = moduloPoly(output, mod);
-	returnable[1] = moduloPoly(poly1, mod);
-	
-	return returnable;
-}
-public static boolean equalsPolyMod(int[] poly1, int[] poly2, int[] polyMod) {
-	int[] remainderOne = longPolyDivision(poly1, polyMod, -1)[1];
-	int[] remainderTwo = longPolyDivision(poly2, polyMod, -1)[1];
-	
-	if(remainderTwo.length > remainderOne.length) {
-		int[] remainderTemp = remainderOne;
-		remainderOne = remainderTwo;
-		remainderTwo = remainderTemp;
-	}
-	
-	int leadingZeroes = remainderOne.length - remainderTwo.length;
-	int index = 0;
-	
-	for(int i = 0; i < remainderOne.length; i++)
-	{
+	public static boolean equalsPolyMod(int[] poly1, int[] poly2, int[] polyMod) {
+		int[] remainderOne = longPolyDivision(poly1, polyMod, -1)[1];
+		int[] remainderTwo = longPolyDivision(poly2, polyMod, -1)[1];
 		
-		int tDigit = 0;
-		
-		if(leadingZeroes > 0) {
-			tDigit = 0;
-		}else {
-			tDigit = remainderTwo[index];
-			index++;
+		if(remainderTwo.length > remainderOne.length) {
+			int[] remainderTemp = remainderOne;
+			remainderOne = remainderTwo;
+			remainderTwo = remainderTemp;
 		}
 		
-		if(tDigit != remainderOne[i]) {
-			return false;
+		int leadingZeroes = remainderOne.length - remainderTwo.length;
+		int index = 0;
+		
+		for(int i = 0; i < remainderOne.length; i++)
+		{
+			
+			int tDigit = 0;
+			
+			if(leadingZeroes > 0) {
+				tDigit = 0;
+			}else {
+				tDigit = remainderTwo[index];
+				index++;
+			}
+			
+			if(tDigit != remainderOne[i]) {
+				return false;
+			}
 		}
+		return true;
 	}
-	return true;
-}
 	public static int[] moduloPoly(int[] poly, int mod) {
 		
 		int[] output = new int[poly.length];
